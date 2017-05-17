c1222addabf64da033cfe7874f75415bc0567ece
Maurizio Branca <mbranca@tierratelematics.com>
2017-05-17 18:18:18 +0200

    Add environment to Lambda name
    This is needed to share the same AWS region with multiple application environments.

--- modules/lambda/event-source-mapping/main.tf
+++ modules/lambda/event-source-mapping/main.tf
@@ -8,7 +8,7 @@ terraform {
 
 resource "aws_lambda_event_source_mapping" "event_source_mapping" {
   event_source_arn  = "${var.event_source_arn}"
-  function_name     = "${format("arn:aws:lambda:%s:%s:function:%s_%s:%s", var.aws["region"], var.aws["account_id"], var.project, var.function_name, var.function_alias)}"
+  function_name     = "${format("arn:aws:lambda:%s:%s:function:%s_%s_%s:%s", var.aws["region"], var.aws["account_id"], var.project, var.environment, var.function_name, var.function_alias)}"
   batch_size        = "${var.batch_size}"
   starting_position = "${var.starting_position}"
   enabled           = "${var.enabled}"
